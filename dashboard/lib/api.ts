e2abeb8c29936cfc6efbf7bb60fd8226d2c8ac2b
Luke Kenny <lckenny@outlook.com>
2026-03-06 05:20:50 +1100

    Updated memory structure logic

--- dashboard/lib/api.ts
+++ dashboard/lib/api.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://172.16.10.140:8080'
 
 export const getHeaders = () => {
-    const apiKey = process.env.NEXT_PUBLIC_API_KEY
+    const apiKey = '7pCxz4TIdh2BPHpKegfSCzpsBiDjSCFE'
     return {
         'Content-Type': 'application/json',
         ...(apiKey && { 'x-api-key': apiKey }),
